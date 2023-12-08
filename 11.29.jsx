391f4bcb6571fb5e67be7c0862f68024f56d5296
sungyehyw <153262959+sungyehyw@users.noreply.github.com>
2023-12-08 15:49:01 +0900

    Add files via upload

---
+++ 11.29.jsx
@@ -0,0 +1,31 @@
+// 객체 자료형
+let number = new Number(273);
+let string = new String('안녕하세요');
+let boolean = new Boolean(true);
+
+// 자료형을 출력합니다.
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof boolean);
+
+// 변수를 선언합니다.
+let number = 273.5210332;
+
+// 출력합니다.
+console.log(number.toFixed(1));
+console.log(number.toFixed(4));
+
+// 변수를 선언합니다.
+let string = 'abcdefg'; 
+
+// 출력합니다.
+string = string.toUpperCase();
+console.log(string);
+
+// 변수를 선언합니다.
+let string = '안녕하세요. 좋은 아침입니다.';
+
+// 문자열 내뭅에 '아침'이라는 문자열이 있는지 확인합니다.
+if(string.indexOf('아침') >= 0){
+    console.log('좋은 아침이에요...!');
+}
