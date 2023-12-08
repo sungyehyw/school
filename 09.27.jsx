b69aa44c404510f919f9bdeb5fd74bc877a8eb41
sungyehyw <153262959+sungyehyw@users.noreply.github.com>
2023-12-08 15:31:32 +0900

    Add files via upload

---
+++ 09.27.jsx
@@ -0,0 +1,48 @@
+console. log('my' + 'cat');
+console. log('1' + 2);
+console. log('string literals: 1 + 2 = ${1 + 2}');
+console.log("ellie's \n\tbook");
+
+console. log(1 + 2);
+console. log(1 - 1);
+console. log(1 / 1);
+console. log(1 * 1);
+console. log(5 % 2);
+console. log(2 ** 3);
+
+let counter = 2;
+const preIncrement = counter ++;
+
+console.log('preIncrement: ${postIncrement}, counter: ${counter}')
+const postIncrement = counter++;
+console.log('preIncrement: ${postIncrement}, counter: ${counter}')
+const preDecrement = --counter;
+console.log('preIncrement: ${postIncrement}, counter: ${counter}')
+const postDecrement = counter--;
+console.log('preIncrement: ${postIncrement}, counter: ${counter}')
+
+let x = 3;
+let y = 6;
+x += y; // x = x + y
+x -= y;
+x *= y;
+x /= y;
+
+console. log(10 < 6);
+console. log(10 <= 6);
+console. log(10 > 6);
+console. log(10 >= 6);
+
+const value1 = flase;
+const value2 = 4 < 2;
+
+
+
+//! (not)
+console. log(!value1);
+
+console. log(stringFive == numberFive);
+console. log(stringFive != numberFive);
+
+console. log(stringFive === numberFive);
+console. log(stringFive !== numberFive);
